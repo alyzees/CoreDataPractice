5339d74976724ccbe7e40f47a16b8c332cfc9e15
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-17 15:13:59 -0700

    core data

--- CoreDataPractice/ContentView.swift
+++ CoreDataPractice/ContentView.swift
@@ -10,7 +10,9 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    
 
+    // APIs!!
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
